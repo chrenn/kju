911e4a513ccbcea62f042e31c071aa0861894f2f
bequadro <30233933+bequadro@users.noreply.github.com>
2018-03-16 21:12:51 +0100

    This is nice.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-const { Chrome } = require('navalia');
+const puppeteer = require('puppeteer');
 const notifier = require('node-notifier');
 
 const Logger = require('./logger');
@@ -12,7 +12,7 @@ const GOOGLE_COOKIES = require('./cookies');
 const logger = new Logger();
 logger.intro(CONFIG.instances);
 
-const splash = async (instance, config) => {
+const splash = async (browser, instance, config) => {
 
 	try {
 	
@@ -20,45 +20,40 @@ const splash = async (instance, config) => {
 		let viewportX = Math.floor(400 * (1 + 1.0 * Math.random()));
 		let viewportY = Math.floor(400 * (1 + 0.5 * Math.random()));
 
-		let chrome = new Chrome({
-			timeout: 60000,
-			flags: {
-				'headless': config.headless,
-				'disableSync': true,
-				'disable-infobars': true,
-				//'disable-web-security': true,
-				'enable-translate-new-ux': true,
-				//'disable-translate-new-ux': true,
-				'no-default-browser-check': true,
-				'window-size': `${viewportX},${viewportY}`,
-				'user-agent': userAgent,
-				'user-data-dir': path.resolve('tmp', 'chrome_' + instance),
-				'profile-directory': 'PROFILE_' + instance
-			},
-		});
+		
+		const page = await browser.newPage();
+		page.setUserAgent(userAgent);
 
-		const thruSplash = async () => {
-			// let captchaClass = await chrome.exists('.g-recaptcha', { wait: false });
-			// let captchaId = await chrome.exists('#g-recaptcha', { wait: false });
-			// let captchaKey = await chrome.evaluate(() => window.CAPTCHA_KEY);
-			// return (captchaClass || captchaId || captchaKey);
-			return await chrome.evaluate(() => typeof grecaptcha !== "undefined");
+		// let chrome = new Chrome({
+		// 	timeout: 60000,
+		// 	flags: {
+		// 		'headless': config.headless,
+		// 		'disableSync': true,
+		// 		'disable-infobars': true,
+		// 		//'disable-web-security': true,
+		// 		'enable-translate-new-ux': true,
+		// 		//'disable-translate-new-ux': true,
+		// 		'no-default-browser-check': true,
+		// 		'window-size': `${viewportX},${viewportY}`,
+		// 		'user-agent': userAgent,
+		// 		'user-data-dir': path.resolve('tmp', 'chrome_' + instance),
+		// 		'profile-directory': 'PROFILE_' + instance
+		// 	},
+		// });
+
+
+
+		await page.goto(config.splashURL);
+
+		while (!(await page.evaluate(() => typeof grecaptcha !== "undefined"))) {
+			logger.info(instance, await page.evaluate(() => document.title));
+			await page.waitFor(config.timeout * 1000);
+			if (config.reload) await page.goto(config.splashURL);
 		}
 
-		await chrome.goto('http://www.google.com/404');
-		for (let cookie of GOOGLE_COOKIES) {
-			await chrome.cookie(cookie.name, cookie.value);
-		}
+		//await page.bringToFront();
 
-		await chrome.goto(config.splashURL);
-
-		while (!(await thruSplash())) {
-			logger.info(instance, await chrome.evaluate(() => document.title));
-			await chrome.wait(config.timeout * 1000);
-			if (config.reload) await chrome.goto(config.splashURL);
-		}
-
-		let cookieJar = await chrome.cookie();
+		let cookieJar = await page.cookies();
 		let gceeqs = '';
 		for (let cookie of cookieJar) {
 			if (cookie.name == 'gceeqs') {
@@ -69,19 +64,22 @@ const splash = async (instance, config) => {
 		let saveDir = path.resolve('html', Date.now().toString());
 		await fs.ensureDir(saveDir);
 
-		await chrome.save(path.resolve(saveDir, 'index.html'));
+		await fs.outputFile(path.resolve(saveDir, 'index.html'), await page.content());
 		await fs.outputFile(path.resolve(saveDir, 'cookies.json'), JSON.stringify(cookieJar));
 		await fs.outputFile(path.resolve(saveDir, 'ua.txt'), userAgent);
-		await fs.outputFile(path.resolve(saveDir, 'body.png'), await chrome.screenshot('body'));
+		//await fs.outputFile(path.resolve(saveDir, 'body.png'), await page.screenshot('body'));
 
 		logger.success(instance, gceeqs, userAgent);
 
 		notifier.notify({
 			title: '❯❯❯_ Kju',
 			icon: path.resolve('icon.png'),
-			contentImage: path.resolve(saveDir, 'body.png'),
+			//contentImage: path.resolve(saveDir, 'body.png'),
 			message: `Through Splash on Instance ${instance}!`,
 			sound: 'Hero',
+			timeout: 60000
+		}, async (err, res, data) => {
+			if (res == 'activate') await page.bringToFront();
 		});
 	
 	} catch (err) {
@@ -93,9 +91,29 @@ const splash = async (instance, config) => {
 }
 
 const main = async () => {
+	console.log(puppeteer.defaultArgs());
+	const browser = await puppeteer.launch({
+		headless: false,
+		executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+		args: [
+			'--disable-sync',
+			'--disable-infobars',
+			'--enable-translate-new-ux',
+			'--no-default-browser-check'
+		]
+	});
+	const page = await browser.newPage();
+	await page.goto('http://www.google.com/404');
+	for (let cookie of GOOGLE_COOKIES) {
+		await page.setCookie({
+			name: cookie.name,
+			value: cookie.value
+		});
+	}
+	await page.close();
 	for (let i = 1; i <= CONFIG.instances; i++) {
 		await utils.timeout(500);
-		splash(i.pad(), CONFIG);
+		splash(browser, i.pad(), CONFIG);
 	}
 }
 
